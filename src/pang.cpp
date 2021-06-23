9ab75040b84303f5b7bef66597cff78e4c5294b2
mel-mouk <contact@achiev.fr>
2021-06-23 18:34:50 +0200

    Basic game loop

--- src/pang.cpp
+++ src/pang.cpp
@@ -1,17 +1,33 @@
 #include "pang.h"
 
 sf::RenderWindow Pang::_window;
+sf::Clock Pang::_clock;
 
 void Pang::start() {
     if (_window.isOpen()) return;
 
     _window.create(sf::VideoMode(Pang::SCREEN_WIDTH, Pang::SCREEN_HEIGHT, 32), "Pang");
+    _window.setFramerateLimit(60);
 
+    gameLoop();
+}
+
+void Pang::gameLoop() {
     while (_window.isOpen()) {
+        float timeElapsed = _clock.restart().asSeconds();
+        _window.clear(sf::Color(255, 255, 255));
+
+        // Handle input
         sf::Event event;
         while (_window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) { _window.close(); }
+            // else handle controls input
         }
+
+        // Update our entities
+
+        // Draw our new entities
+
         _window.display();
     }
 }
--- src/pang.h
+++ src/pang.h
@@ -8,9 +8,11 @@ public:
     const static int SCREEN_WIDTH = 2048;
     const static int SCREEN_HEIGHT = 1536;
     static void start();
+    static void gameLoop();
 
 private:
     static sf::RenderWindow _window;
+    static sf::Clock _clock;
 };
 
 #endif //PANG_PANG_H
