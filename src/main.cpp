0df1c90f6f3bdfdfa83163cd1c80946e1e1aa963
mel-mouk <contact@achiev.fr>
2021-06-23 15:04:36 +0200

    Hello world

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int main() {
+    std::cout << "Welcome to Pang !" << std::endl;
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,6 @@
+#ifndef SFML_PONG_MAIN_H
+#define SFML_PONG_MAIN_H
+
+#include <iostream>
+
+#endif //SFML_PONG_MAIN_H
