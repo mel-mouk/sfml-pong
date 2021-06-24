d68ee78a0da33e72518e741b457bb505a3a5ae31
mel-mouk <contact@achiev.fr>
2021-06-24 21:50:16 +0200

    Display the player

--- src/states/playing-state.cpp
+++ src/states/playing-state.cpp
@@ -1,9 +1,19 @@
 #include "playing-state.h"
 
-void PlayingState::init() { }
+void PlayingState::init() {
+    if (!_player1Texture.loadFromFile("../assets/paddle.png")) {
+        std::cout << "Error while loading paddle assets" << std::endl;
+        return;
+    }
+
+    _player1Sprite.setTexture(_player1Texture);
+    _player1Sprite.setPosition(150, 718);
+}
 
 void PlayingState::handleInput(sf::Event *event) { }
 
 void PlayingState::update(float timeElapsed) { }
 
-void PlayingState::draw(sf::RenderWindow *window) { }
+void PlayingState::draw(sf::RenderWindow *window) {
+    window->draw(_player1Sprite);
+}
--- src/states/playing-state.h
+++ src/states/playing-state.h
@@ -1,6 +1,7 @@
 #ifndef PANG_PLAYING_STATE_H
 #define PANG_PLAYING_STATE_H
 
+#include <iostream>
 #include "game-state.h"
 
 class PlayingState: public GameState {
@@ -9,6 +10,10 @@ public:
     void handleInput(sf::Event *event) override;
     void update(float timeElapsed) override;
     void draw(sf::RenderWindow *window) override;
+
+private:
+    sf::Texture _player1Texture;
+    sf::Sprite _player1Sprite;
 };
 
 #endif //PANG_PLAYING_STATE_H
